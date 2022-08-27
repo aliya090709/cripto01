cda7d4d129eba43c79cf4266c857e5366e38cdd8
aliya090709 <aliyasamsalieva734@gmail.com>
2022-08-27 09:23:59 -0700

    initial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+class Swiper {
+    constructor(s, param2) {
+
+    }
+
+}
+new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
